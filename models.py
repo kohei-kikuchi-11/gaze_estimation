2948229ce3f1bff0c0cfcfda0415d9574cf003fd
kohei-kikuchi-11 <kohei.k.06.ub@gmail.com>
2026-04-04 22:24:04 +0900

    3rd-fix

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ def to_nchw(image):
     image = np.transpose(image, (2, 0, 1))[np.newaxis, :]
     return image
 
+# ベースモデル
 class BaseModel:
     def __init__(self, model_path, device="CPU")
         self.core = Core()
@@ -36,10 +37,10 @@ class FaceDetector(BaseModel):
         for det in outputs[0][0]:
             conf = det[2]
             if conf > conf_th:
-            xmin = int(det[3] * w)
-            ymin = int(det[4] * h)
-            xmax = int(det[5] * w)
-            ymax = int(det[6] * h)
+                xmin = int(det[3] * w)
+                ymin = int(det[4] * h)
+                xmax = int(det[5] * w)
+                ymax = int(det[6] * h)
             faces.append((xmin,ymin, xmax, ymax))
         return faces
 
@@ -63,4 +64,48 @@ class LandmarkDetector(BaseModel):
 # 頭部推定クラス
 class HeadPoseEstimator(BaseModel):
     def postprocess(self, outputs):
+        yaw = outputs["angle_y_fc"][0][0]
+        pitch = outputs["angle_p_fc"][0][0]
+        roll = outputs["angle_t_fc"][0][0]
+
+        return yaw, pitch, roll
+
+# 視線推定クラス
+class GazeEstimation(BaseModel):
+    def preprocess(self, left_eye, right_eye, head_pose):
+        # 目画像
+        le = to_nchw(cv2.resize(left_eye, (60, 60)))
+        re = to_nchw(cv2.resize(right_eye, (60, 60)))
+
+        hp = np.array(head_pose).reshape(1, 3)
+
+        return {
+            "left_eye_image": le,
+            "right_eye_image": re,
+            "head_pose_angles": hp
+        }
+    
+    def infer(self, inputs):
+        return self.compiled_model(inputs)
+    
+    def postprocess(self, outputs):
+        return outputs[self.output_layer][0]
+
+# パイプライン本体
+import cv2
+
+class GazePipeline:
+    def __init__(self, model_paths):
+        self.face = FaceDetector(model_paths["face"])
+        self.landmark = LandmarkDetector(model_paths["landmark"])
+        self.headpose = HeadPoseEstimator(model_paths["headpose"])
+        self.gaze = GazeEstimation(model_paths["gaze"])
+    
+    def process_frame(self, frame):
+        # Face Detection
+        face_input = self.face.preprocess(frame)
+        face_out = self.face.infer(face_input)
+        faces = self.face.postprocess(face_out, frame)
+
+        results = []
         
