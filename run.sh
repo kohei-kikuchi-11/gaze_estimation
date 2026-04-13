b28eb129709ab8b60ffeae5aa00699cc44a83a48
kohei-kikuchi-11 <kohei.k.06.ub@gmail.com>
2026-04-13 13:31:26 +0900

    update

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ CONTAINER_ID=$(docker compose run -d gaze_estimation --input "$1")
 echo "Container ID: $CONTAINER_ID"
 
 # 終了時に削除
-trap "docker rm -f $CONTAINER_ID 2>/dev/null" EXITs
+trap "docker rm -f $CONTAINER_ID 2>/dev/null" EXIT
 
 # コンテナ終了待ち
 docker wait $CONTAINER_ID
